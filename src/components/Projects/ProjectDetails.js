206bdeb238b5939d598b2eb7fa01e614593a90b6
GJ <joshi700@gmail.com>
2024-10-11 21:41:30 +0530

    Update ProjectDetails.js

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -3,17 +3,19 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const projects = [
-  { id: 1, title: 'CheckFree RXP', description: 'Integrating Fiservs Bill Pay solution - CheckFree RXP to Stifels Online Banking Platform' },
-  { id: 2, title: 'The Vault ', description: 'Central Business Intelligence and Service Delivery Framework. Problem Statement
-    Stifel had Bill Pay application
-    <br>
-    NPS scores were not too good for that Bill Payment product
-    The overall percentage of users enrolled in Bill Pay was 11 percent
-    Stifel Bank pushed campaigns and promotions in the past and have found some lift average 2 to 3 percent
-    Really close to 50 percent inactive bill pay accounts
-    Inactive bill pay accounts Subscribers that have not paid a bill in 4 months ' },
+  {
+    id: 1,
+    title: 'CheckFree RXP',
+    description: 'Integrating Fiserv\'s Bill Pay solution - CheckFree RXP to Stifel\'s Online Banking Platform'
+  },
+  {
+    id: 2,
+    title: 'The Vault',
+    description: 'Central Business Intelligence and Service Delivery Framework.\nProblem Statement:\nStifel had Bill Pay application.\nNPS scores were not too good for that Bill Payment product.\nThe overall percentage of users enrolled in Bill Pay was 11 percent.\nStifel Bank pushed campaigns and promotions in the past and have found some lift, averaging 2 to 3 percent.\nReally close to 50 percent inactive bill pay accounts.\nInactive bill pay accounts are Subscribers that have not paid a bill in 4 months.'
+  },
   // Add more projects as needed
 ];
+
 
 const ProjectDetails = () => {
   const { id } = useParams();
