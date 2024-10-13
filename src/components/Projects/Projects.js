2acca530fef4d875d1d86379fb4b6ef48cdacaa2
GJ <joshi700@gmail.com>
2024-10-13 21:31:01 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ import Particle from "../Particle";
 import "../../App.css";
 
 const projects = [
-  { id: 1, title: 'Jebel Ali Free Trade Zone Authority - JAFZA', description: 'Mobile App is designed to provide a seamless experience in the free zone, enabling customers to manage their operations efficiently and stay ahead in the competitive business landscape.' },
+  { id: 1, title: 'Jebel Ali Free Trade Zone Authority', description: 'Mobile App designed to provide a seamless experience in the free zone' },
   { id: 2, title: 'The Vault ', description: 'Central Business Intelligence and Service Delivery Framework' },
   // Add more projects as needed
 ];
