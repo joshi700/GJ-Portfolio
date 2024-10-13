0e5676178ffb5cbd568a06bd36382ddc6acaac32
GJ <joshi700@gmail.com>
2024-10-13 22:33:21 +0530

    Update ProjectDetails.js

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -81,8 +81,98 @@ const projects = [
   {
     id: 2,
     title: 'The Vault',
-    description: 'Central Business Intelligence and Service Delivery Framework.<br><br><br>Problem Statement:<br><br><br>Stifel had Bill Pay application.<br>NPS scores were not too good for that Bill Payment product.<br>The overall percentage of users enrolled in Bill Pay was 11 percent.<br>Stifel Bank pushed campaigns and promotions in the past and have found some lift, averaging 2 to 3 percent.<br>Really close to 50 percent inactive bill pay accounts.<br>Inactive bill pay accounts are Subscribers that have not paid a bill in 4 months.'
-  },
+    description: '<strong>Central Business Intelligence and Service Delivery Framework.<br><br><br></strong>
+<strong>Problem Statement:</strong><br><br><br>
+<ul>
+<li>Stifel had Bill Pay application.<br>NPS scores were not too good for that Bill Payment product.<br></li>
+<li>The overall percentage of users enrolled in Bill Pay was 11 percent.<br></li>
+<li>Stifel Bank pushed campaigns and promotions in the past and have found some lift, averaging 2 to 3 percent.<br></li>
+<li>Really close to 50 percent inactive bill pay accounts.<br></li>
+    - Inactive bill pay accounts are Subscribers that have not paid a bill in 4 months.
+</ul>
+
+<strong>Banks Transformational Leader</strong><br><br>
+<ul>
+	<li>How is it aligned to their Banks goals?<br></li>
+	<li>How will it be better for business?<br></li>
+	<li>How much is the value?<br><li>
+</ul>
+
+
+<strong>ROI Estimate - Metrics</strong><br><br>
+
+<strong>Maximize Customer Satisfaction - Increase Consumer Engagement by 30%</strong><br><br>
+<ul>
+	<li>NPS scores were not too good for Bill Payment product<br></li>
+</ul>
+
+
+<strong>Increase Customer Sticky ness</strong><br><br>
+<ul>
+	<li>Once customers has all of their bills setup, its more trouble to change banks.<br></li>
+	<li>This reduces the customer turnover rate, which lowers their costs<br></li>
+</ul>
+
+
+<strong>More companies are linked to Checkfree RXP</strong><br><br>
+<ul>
+	<li>CheckFree RXP supports the delivery of e-bills from more than 600 companies including utilities, insurers, mobile providers and credit card companies, enabling consumers to both receive and pay bills at their financial institution website.<br></li>
+	<li>With Fiserv the Inactive Bill Payment user levels out at 25%, with 75% being active (Active Users paid at least one bill in that given month). <br></li>
+</ul>
+
+<strong>Bank Charges</strong><br><br>
+<ul>
+	<li>While many banks charge a 5% fee, the company’s processing fees are only 1.25% of the total purchase and 1% of that is passed through to the customer as a cash back reward. <br></li>
+	<li>All a customer needs to do to purchase their coffee or a new shirt with AeroPay is sync their bank account and the app will automatically connect when the customer enters a participating store.<br></li>
+</ul>
+
+<strong>Three major goals in a business</strong><br><br>
+<ul>
+	<li>Increase revenue. <br></li>
+	<li>Lower costs.<br></li>
+	<li>Reduce losses.<br></li>
+</ul>
+
+<strong>Payment Conversion Rate</strong><br><br>
+<ul>
+	<li>The more detail Checkfree RXP can give about the reasons behind declined transactions, the better. <br></li>
+	<li>For example, if an unusually high number of transactions are declined due to possible fraud, Checkfree RXP should be working harder behind the scenes to prevent this from happening.<br></li>
+	<li>The issues that come to the forefront are the ones you should focus on addressing.<br></li>
+	<li>This metric tells you how many transactions on the whole were declined.<br></li>
+	<li>If 80 out of 100 transactions were successful, you have an 80% payment conversion rate.There are many reasons why transactions are declined<br></li>
+	incorrect card information
+	insufficient funds
+	invalid card numbers
+	suspicion of fraud
+</ul>
+
+<strong>Define number of users</strong><br><br>
+<ul>
+	<li>For a variety of reasons, some of your charges may fail to succeed.  <br></li>
+	<li>Whether it is a bank-declined payment, a blocked by Stripe payment or just an invalid API call, you should be aware of the total declined amount and of their type in order to proceed to the appropriate actions.  <br></li>
+
+</ul>
+
+<strong>Top Customers with Total Spend</strong><br><br>
+<ul>
+	<li>Knowing your most valuable and delighted customers is important for every company.   <br></li>
+	<li>Good customers will make big purchases and generate a substantial portion of your revenues.  <br></li>
+</ul>
+
+
+
+<strong>New Customers over time</strong><br><br>
+<ul>
+	<li>The steady growth of your customer base in order to maximize its revenue is the ulterior goal of every company.  <br></li>
+</ul>
+
+<strong>Disputes & Fraud</strong><br><br>
+<ul>
+	<li>When it comes to disputes it is important to monitor both their number and their status along with their type. <br></li>
+	<li>Depending on the dispute’s type you may perform certain actions and adduce certain evidence. <br></li>
+</ul>
+
+' },
   
   // Add more projects as needed
 ];
