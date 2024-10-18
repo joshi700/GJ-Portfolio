3b64ad297a8e5ccf9abb295dfe63a254e797f607
GJ <joshi700@gmail.com>
2024-10-18 23:23:17 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -100,7 +100,7 @@ function NavBar() {
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
-            </Nav.Item> */}
+            </Nav.Item> 
 
             <Nav.Item className="fork-btn">
               <Button
@@ -111,7 +111,7 @@ function NavBar() {
                 <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
                 <AiFillStar style={{ fontSize: "1.1em" }} />
               </Button>
-            </Nav.Item>
+            </Nav.Item>*/}
           </Nav>
         </Navbar.Collapse>
       </Container>
